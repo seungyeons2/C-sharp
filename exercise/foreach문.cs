62d87bbd994834c0a6fd8294bb35a9dcf85ecb7d
seungyeons2 <seungyeonsi913@gmail.com>
2023-01-17 16:38:02 +0900

    foreach

---
+++ exercise/foreach문.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[] array3 = { 4, 5, 6 };
+
+        foreach(int item in array3)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
+
